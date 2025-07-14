75d2063fe30cc17d4ed9a9908ce576810b4d24c1
felipe <felipe.lleuful@hotmail.com>
2025-07-13 20:23:59 -0400

    Creacion de mi primer proyecto Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Extra } from './pages/extra/extra';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'extra', component: Extra},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from './pages/nav/nav';
+import { Footer } from './pages/footer/footer'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Nav, Footer],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('proyectoAngular');
+  protected readonly title = signal('Felipe');
 }
