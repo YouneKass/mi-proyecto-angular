f4b6ed5ddd19a02217cb4d5521020b07240f3894
felipe <felipe.lleuful@hotmail.com>
2025-08-17 21:59:47 -0400

    Correccion de errores

--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -3,8 +3,6 @@ import { LayoutComponent } from './layout.component';
 import { Home } from '../pages/home/layout/home.component';
 import { Contact } from '../pages/contact/contact.component';
 import { Inventario } from '../pages/inventario/inventario.component';
-import { MarvelListComponent } from '../pages/marvel-list/layout/marvel-list.component';
-
 
 export const layoutRoutes: Routes = [
   {
@@ -14,7 +12,6 @@ export const layoutRoutes: Routes = [
       {path: '', component: Home},
       {path: 'inventario', component: Inventario},
       {path: 'contact', component: Contact},
-      {path: 'marvelList', component: MarvelListComponent}
     ]
   }
 ];
